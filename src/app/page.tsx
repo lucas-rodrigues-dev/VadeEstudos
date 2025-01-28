8f45d9e6f881a74a4ee8ef9dd6fc98dc97dccb47
Lucas Rodrigues <lucas.rodmendes@gmail.com>
2025-01-28 02:56:02 -0300

    updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,95 @@
+"use client";
 import Image from "next/image";
+import Swal from "sweetalert2";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+export default function Home () {
+		const currentYear = new Date().getFullYear();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+		const handleAlert = (message: string) => {
+			Swal.fire({
+				title: "Aguarde!",
+				text: message,
+				icon: "info",
+				confirmButtonText: "Entendi",
+				confirmButtonColor: "#8b1c41",
+			});
+		};
+
+	return (
+		<div className="min-h-screen bg-gradient-to-r from-[#fff0ec] to-[#e6bab6] flex flex-col items-center text-center font-sans">
+		{/* Imagem de Cabeçalho */}
+		<div className="my-8">
+			<Image
+			src="/VadeEstudosMonitorias.png" 
+			alt="Vade Estudos"
+			width={200}
+			height={200}
+			className="rounded-full"
+			/>
+		</div>
+
+		{/* Configurações do Instagram */}
+		<div className="text-center leading-snug mb-4">
+			<div className="text-[17px] mb-4">
+			<a
+				href="https://www.instagram.com/vade.estudos/"
+				className="text-black no-underline hover:underline"
+			>
+				@vade.estudos
+			</a>
+			</div>
+			<p className="text-[16px] text-black font-bold">
+			⚖️ Transformando sua jornada no Direito
+			</p>
+
+			<p className="text-[16px] text-black font-bold">
+			📚 Resumos em PDF | Monitorias | ABNT
+			</p>
+
+			<p className="text-[16px] text-black font-bold">
+			🎯 Otimize seus estudos e organize sua rotina!
+			</p>
+		</div>
+
+		{/* Links de Botões */}
+		<div className="w-full flex flex-col items-center">
+			<a 
+			className="w-[70%] bg-none text-black px-4 py-4 my-2 border border-[#8b1c41] rounded-full cursor-pointer font-medium transition-all duration-500 hover:bg-[#80092f] hover:text-[#ffc3d7]"
+			href="https://w.app/hdmsfy"
+			target="_blank">
+				<button>
+					👋🏻 Entre em Contato Comigo
+				</button>
+			</a>
+
+			<a 
+			className="w-[70%] bg-none text-black px-4 py-4 my-2 border border-[#8b1c41] rounded-full cursor-pointer font-medium transition-all duration-500 hover:bg-[#80092f] hover:text-[#ffc3d7]"
+			href="#"
+			target="_blank"
+			onClick={() => handleAlert("Em Breve Lançaremos nosso Catálogo, enquanto isso você pode me contatar para tirar dúvidas sobre o Vade ou outros assuntos 😉")}>
+				<button>
+					📚 Acesse Nosso Catálogo e Garanta o seu Material de Estudos
+				</button>
+			</a>
+
+			<a 
+			className="w-[70%] bg-none text-black px-4 py-4 my-2 border border-[#8b1c41] rounded-full cursor-pointer font-medium transition-all duration-500 hover:bg-[#80092f] hover:text-[#ffc3d7]"  
+			href="https://www.linkedin.com/in/mabe-carvalho2210/"
+			target="_blank">
+				<button>
+					💻 Me siga no LinkedIn
+				</button>
+			</a>
+		</div>
+
+		{/* Rodapé */}
+		<footer className="text-black text-[1rem] mt-6">
+			© {currentYear} Vade Estudos. Todos os direitos reservados.
+		</footer>
+
+		<div className="text-black text-[1rem] mt-1 font-bold">
+			Desenvolvido Com ❤️ por <a href="https://www.instagram.com/lucas.dev.exe/" className="text-black no-underline hover:underline">Lucas</a>
+		</div>
+		</div>
+	);
 }
