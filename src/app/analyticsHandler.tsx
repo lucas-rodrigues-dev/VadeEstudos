3d637b9e0321dd4c918195468bfb85e03b3a4aa1
Lucas Rodrigues <lucas.rodmendes@gmail.com>
2025-01-29 01:56:46 -0300

    updated Analytics Integration

--- src/app/analyticsHandler.tsx
+++ src/app/analyticsHandler.tsx
@@ -1,29 +1,26 @@
-"use client";
+import Script from 'next/script';
+import { GA_TRACKING_ID } from './lib/gtag';
 
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+const Analytics = () => (
+	<>
+	<Script 
+		strategy = "afterInteractive"
+		src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+	/>
+	<Script 
+		strategy = "afterInteractive"
+		dangerouslySetInnerHTML={{
+			__html: `
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+				gtag('config', '${GA_TRACKING_ID}', {
+					page_path: window.location.pathname,
+				});
+			`
+		}}
+	/>
+  </>
+)
 
-export default function GoogleAnalyticsHandler() {
-  const router = useRouter();
-  const isProduction = process.env.NODE_ENV === "production"; // Verifica o ambiente
-
-  useEffect(() => {
-    if (!isProduction) return; // Não executa no ambiente de desenvolvimento
-
-    const handleRouteChange = (url: string) => {
-      if (typeof window.gtag === "function") {
-        window.gtag("config", process.env.NEXT_PUBLIC_GA_ID, {
-          page_path: url,
-        });
-      }
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router, isProduction]);
-
-  return null;
-}
+export default Analytics;
