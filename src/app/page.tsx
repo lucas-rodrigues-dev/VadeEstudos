6d80eb3d7d8cdcf4afc651700649c670b1fbb50b
Lucas Rodrigues <65752473+lucas-rodrigues-dev@users.noreply.github.com>
2025-02-14 17:37:59 -0300

    updated all buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import Swal from 'sweetalert2';
 
-
 export default function Home () {
 		const currentYear = new Date().getFullYear();
 
@@ -34,9 +33,8 @@ export default function Home () {
 		<div className="text-center leading-snug mb-4">
 			<div className="text-[17px] mb-4">
 			<a
-				href="https://www.instagram.com/vade.estudos/"
+				onClick = {() => window.open('https://www.instagram.com/vade.estudos/', '_blank', 'noreferrer noopener')}
 				className="text-black no-underline hover:underline"
-				rel="noreferrer noopener"
 			>
 				@vade.estudos
 			</a>
@@ -57,42 +55,38 @@ export default function Home () {
 		{/* Links de Botões */}
 		<div className="w-full flex flex-col items-center">
 			<a 
-			className="w-[70%] bg-none text-black px-4 py-4 my-2 border border-[#8b1c41] rounded-full cursor-pointer font-medium transition-all duration-500 hover:bg-[#80092f] hover:text-[#ffc3d7]"
-			href="https://api.whatsapp.com/send?phone=5561981576620&text=Ol%C3%A1%2C%20vim%20pelo%20seu%20linktree%20e%20tenho%20interesse%20em%20seus%20produtos"
-			rel="noreferrer noopener"
-			target="_blank">
-				<button>
+			className="w-[70%] bg-none text-black px-4 py-4 my-2 border border-[#8b1c41] rounded-full cursor-pointer font-medium transition-all duration-500 hover:bg-[#80092f] hover:text-[#ffc3d7]">
+				<button
+					onClick = {() => window.open('https://api.whatsapp.com/send?phone=5561981576620&text=Ol%C3%A1%2C%20vim%20pelo%20seu%20linktree%20e%20tenho%20interesse%20em%20seus%20produtos', '_blank', 'noreferrer noopener')}
+				>
 					👋🏻 Entre em Contato Comigo
 				</button>
 			</a>
 
 			<a 
 			className="w-[70%] bg-none text-black px-4 py-4 my-2 border border-[#8b1c41] rounded-full cursor-pointer font-medium transition-all duration-500 hover:bg-[#80092f] hover:text-[#ffc3d7]"
-			/*href="https://vade-estudos-store.vercel.app/" */
-			rel="noreferrer noopener"
-			target="_blank"
 			onClick = {showAlert}>
-				<button>
+				<button
+					/* onClick={() => window.open('https://vade-estudos-store.vercel.app/', '_blank', 'noreferrer noopener')} */
+				>
 					🛒 Acesse Nossa Loja e Garanta o seu Material de Estudos
 				</button>
 			</a>
 
 			<a 
-			className="w-[70%] bg-none text-black px-4 py-4 my-2 border border-[#8b1c41] rounded-full cursor-pointer font-medium transition-all duration-500 hover:bg-[#80092f] hover:text-[#ffc3d7]"
-			href="https://chk.eduzz.com/39YBVKOJWO"
-			rel="noreferrer noopener"
-			target="_blank">
-				<button>
+			className="w-[70%] bg-none text-black px-4 py-4 my-2 border border-[#8b1c41] rounded-full cursor-pointer font-medium transition-all duration-500 hover:bg-[#80092f] hover:text-[#ffc3d7]">
+				<button
+					onClick={() => window.open('https://chk.eduzz.com/39YBVKOJWO','_blank', 'noreferrer noopener')}
+				>
 					📚 Compre o seu Studies Planner 2025
 				</button>
 			</a>
 
 			<a 
-			className="w-[70%] bg-none text-black px-4 py-4 my-2 border border-[#8b1c41] rounded-full cursor-pointer font-medium transition-all duration-500 hover:bg-[#80092f] hover:text-[#ffc3d7]"  
-			href="https://www.linkedin.com/in/mabe-carvalho2210/"
-			target="_blank"
-			rel="noreferrer noopener">
-				<button>
+			className="w-[70%] bg-none text-black px-4 py-4 my-2 border border-[#8b1c41] rounded-full cursor-pointer font-medium transition-all duration-500 hover:bg-[#80092f] hover:text-[#ffc3d7]"  >
+				<button
+					onClick={() => window.open('https://www.linkedin.com/in/mabe-carvalho2210/', '_blank', 'noreferrer noopener')}
+				>
 					💻 Me siga no LinkedIn
 				</button>
 			</a>
@@ -104,11 +98,8 @@ export default function Home () {
 		</footer>
 
 		<div className="text-black text-[14px] mt-1 font-bold">
-		Desenvolvido com <a 
-		href="https://nextjs.org/"
-		rel="noreferrer noopener"
-		target="_blank"
-		>
+		Desenvolvido com <a
+		onClick = {() => window.open('https://nextjs.org/', '_blank', 'noreferrer noopener')}>
 			<Image
 				className="inline-block space-x-1 md:space-x-9"
 				src="/nextjs-logo.svg"
